305dc42d095eca0b4b35ccba1c8ad44923d6637f
Onah Leo Chinagorom <leochinagoromonah@gmail.com>
2022-08-29 00:58:07 -0700

    no c or C

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for c in my_string:
+        if c != 'c' and c != 'C':
+            new_string += c
+            
+        else:
+            new_string +=  " "
+    return new_string
+        
+my_string = 'chinccccCCCagodscc'
+
+print(no_c(my_string))
